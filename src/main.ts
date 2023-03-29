6783700ac06de006bff3c082f59803422535afa2
Vitalii Marushko <vi.marushko@gmail.com>
2023-03-29 22:34:44 +0300

    Integrate NestJS application with serverless

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-
-import * as helmet from 'helmet';
-
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
-const port = process.env.PORT || 4000;
+let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,8 +14,17 @@ async function bootstrap() {
   });
   app.use(helmet());
 
-  await app.listen(port);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap().then(() => {
-  console.log('App is running on %s port', port);
-});
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
